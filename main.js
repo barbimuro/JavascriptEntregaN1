4dce6203f1c2c3bc010b9e13e0b28481ad04dd6c
barbimuro <146161145+barbimuro@users.noreply.github.com>
2023-12-23 15:04:47 +0100

    trabajoCoder2

--- main.js
+++ main.js
@@ -1,55 +1,42 @@
 alert("Bienvenidos a GUCCI tienda de moda");
 
+let productos = [
+    { codigo: "1", nombre: "Billetera GUCCI", precio: 550 },
+    { codigo: "2", nombre: "Zapatillas GUCCI", precio: 1700 },
+    { codigo: "3", nombre: "Zapatos GUCCI", precio: 2200 },
+    { codigo: "4", nombre: "Cartera GUCCI", precio: 3000 }
+];
+
 let eleccion = "";
 
-while (eleccion !== "1" && eleccion !== "billetera" && eleccion !== "2" && eleccion !== "zapatillas" && eleccion !== "3" && eleccion !== "zapatos" && eleccion !== "4" && eleccion !== "cartera" ){
-    eleccion = prompt("Elige qué quieres comprar:\n1. Billetera GUCCI - 550USD \n2. Zapatillas GUCCI - 1700USD \n3. Zapatos GUCCI - 2200USD \n4. Cartera GUCCI - 3000USD").toLocaleLowerCase();
-    if (eleccion !== "1" && eleccion !== "billetera" && eleccion !== "2" && eleccion !== "zapatillas" && eleccion !== "3" && eleccion !== "zapatos" && eleccion !== "4" && eleccion !== "cartera") {
+while (!productos.some(producto => producto.codigo === eleccion || producto.nombre.toLowerCase() === eleccion)) {
+    eleccion = prompt("Elige qué quieres comprar:\n1. Billetera GUCCI - 550USD \n2. Zapatillas GUCCI - 1700USD \n3. Zapatos GUCCI - 2200USD \n4. Cartera GUCCI - 3000USD").toLowerCase();
+
+    if (!productos.some(producto => producto.codigo === eleccion || producto.nombre.toLowerCase() === eleccion)) {
         alert("No disponemos de ese producto en la lista, por favor elija uno de la lista");
     }
-}  
+}
 
 let precioFinal = 0;
 
-let esMiembro = prompt("¿Ya eres parte de nuestro club de membresia VIP de GUCCI para que te brindemos un 10% de descuento?").toLocaleLowerCase();
-if (esMiembro =="sí" || esMiembro =="si") {
-    if (eleccion=="1"||eleccion=="billetera"){
-        precioFinal = 550 * 0.9
-    } else if (eleccion=="2" || eleccion=="zapatillas"){
-        precioFinal= 1700*0.9
-    } else if (eleccion=="3" || eleccion=="zapatos"){
-        precioFinal = 2200*0.9
-    } else if (eleccion=="4"||eleccion=="cartera"){
-        precioFinal = 3000*0.9
-    }
-} else if (esMiembro=="no"){
-    let noEsMiembro = prompt ("¿Quieres convertirte en miembro? Responde si o no").toLocaleLowerCase();
-    if (noEsMiembro=="sí"||noEsMiembro=="si"){
-        alert ("¡Felicitaciones, te regalamos un 15% de descuento!")
-        if (eleccion=="1"||eleccion=="billetera"){
-            precioFinal = 550 * 0.85
-        } else if (eleccion=="2" || eleccion=="zapatillas"){
-            precioFinal= 1700*0.85
-        } else if (eleccion=="3" || eleccion=="zapatos"){
-            precioFinal = 2200*0.85
-        } else if (eleccion=="4"||eleccion=="cartera"){
-            precioFinal = 3000*0.85
-        } 
-    } else if (noEsMiembro=="no"){
+let esMiembro = prompt("¿Ya eres parte de nuestro club de membresía VIP de GUCCI para que te brindemos un 10% de descuento?").toLowerCase();
+if (esMiembro === "sí" || esMiembro === "si") {
+    let productoElegido = productos.find(producto => producto.codigo === eleccion || producto.nombre.toLowerCase() === eleccion);
+    precioFinal = productoElegido.precio * 0.9;
+} else if (esMiembro === "no") {
+    let noEsMiembro = prompt("¿Quieres convertirte en miembro? Responde si o no").toLowerCase();
+    if (noEsMiembro === "sí" || noEsMiembro === "si") {
+        alert("¡Felicitaciones, te regalamos un 15% de descuento!")
+        let productoElegido = productos.find(producto => producto.codigo === eleccion || producto.nombre.toLowerCase() === eleccion);
+        precioFinal = productoElegido.precio * 0.85;
+    } else if (noEsMiembro === "no") {
         alert("¡Consideralo más tarde!")
-        if (eleccion=="1"||eleccion=="billetera"){
-            precioFinal = 550
-        } else if (eleccion=="2" || eleccion=="zapatillas"){
-            precioFinal= 1700
-        } else if (eleccion=="3" || eleccion=="zapatos"){
-            precioFinal = 2200
-        } else if (eleccion=="4"||eleccion=="cartera"){
-            precioFinal = 3000
-        } 
+        let productoElegido = productos.find(producto => producto.codigo === eleccion || producto.nombre.toLowerCase() === eleccion);
+        precioFinal = productoElegido.precio;
     } else {
-        alert("Seleccione una respuesta valida")
+        alert("Seleccione una respuesta válida")
     }
-} 
-let precioConIVA = precioFinal*1.21 
-alert("Tu importe final con IVA incluido seria $"+precioConIVA.toFixed(2))
+}
+let precioConIVA = precioFinal * 1.21;
+alert("Tu importe final con IVA incluido sería $" + precioConIVA.toFixed(2));
 
